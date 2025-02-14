4af7c3b93b08f4d144faac30db363f5b74b99a2e
kumarGN69 <kumargn1969@otmail.com>
2025-02-14 08:20:55 -0600

    changes to main handler to save extracted reviews to file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from textblob import TextBlob
+import pandas as pd
 
 from reddit_handler import RedditHandler
 from sentiment_analyzer import SentimentAnalyzer
@@ -20,48 +21,59 @@ if __name__ == "__main__":
     # print(reviews)
     sentiments = SentimentAnalyzer()
     sentiments.assessSentiments(reviews=reviews)
-
     print(f"Positive: {sentiments.positive_sentiments}, Negative:{sentiments.negative_sentiments}, Nuetral: {sentiments.nuetral_sentiments}")
+    for sentiment in sentiments.negative_comments:
+        print(f"neutral:{sentiment}")
+    # print(f"Positive: {sentiments.positive_sentiments}, Negative:{sentiments.negative_sentiments}, Nuetral: {sentiments.nuetral_sentiments}")
     # for comment in sentiments.negative_comments:
     #     print(comment)
 
-# nltk.download("punkt_tab")
-# nltk.download("stopwords")
+    # nltk.download("punkt_tab")
+    # nltk.download("stopwords")
 
-# # Preprocessing
-# def preprocess_text(text):
-#     tokens = word_tokenize(text.lower())
-#     stop_words = set(stopwords.words("english"))
-#     return [word for word in tokens if word.isalnum() and word not in stop_words]
+    # # Preprocessing
+    # def preprocess_text(text):
+    #     tokens = word_tokenize(text.lower())
+    #     stop_words = set(stopwords.words("english"))
+    #     return [word for word in tokens if word.isalnum() and word not in stop_words]
 
-# corpus = sentiments.negative_comments
-# # Tokenize and preprocess text
-# processed_corpus = [preprocess_text(doc) for doc in corpus]
+    # corpus = sentiments.negative_comments
+    # # Tokenize and preprocess text
+    # processed_corpus = [preprocess_text(doc) for doc in corpus]
 
-# # Create dictionary and bag-of-words model
-# dictionary = corpora.Dictionary(processed_corpus)
-# bow_corpus = [dictionary.doc2bow(doc) for doc in processed_corpus]
+    # # Create dictionary and bag-of-words model
+    # dictionary = corpora.Dictionary(processed_corpus)
+    # bow_corpus = [dictionary.doc2bow(doc) for doc in processed_corpus]
 
-# # Train LDA model
-# lda_model = models.LdaModel(bow_corpus, num_topics=5, id2word=dictionary, passes=10)
+    # # Train LDA model
+    # lda_model = models.LdaModel(bow_corpus, num_topics=5, id2word=dictionary, passes=10)
 
-# # Print key themes
-# for idx, topic in lda_model.print_topics(-1):
-#     print(f"🔹 Topic {idx+1}: {topic}")
+    # # Print key themes
+    # for idx, topic in lda_model.print_topics(-1):
+    #     print(f"🔹 Topic {idx+1}: {topic}")
 
-comments = sentiments.nuetral_comments
+    # comments = sentiments.neutral_comments
+    #
+    # for comment in comments:
+    #     # print(comment)
+    #     # Parse and tokenize
+    #     parser = PlaintextParser.from_string(comment, Tokenizer("english"))
+    #
+    #     # Use LSA (Latent Semantic Analysis) Summarizer
+    #     summarizer = LsaSummarizer()
+    #     summary = summarizer(parser.document, 2)  # Extract 2 sentences
+    #
+    #     # Print summary
+    #     print("🔹 Summary:")
+    #     for sentence in summary:
+    #         # pass
+    #         print(sentence)
 
-for comment in comments:
-    # print(comment)
-    # Parse and tokenize
-    parser = PlaintextParser.from_string(comment, Tokenizer("english"))
-
-    # Use LSA (Latent Semantic Analysis) Summarizer
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, 1)  # Extract 2 sentences
-
-    # Print summary
-    print("🔹 Summary:")
-    for sentence in summary:
-        # pass
-        print(sentence)
+    if sentiments.neutral_comments:
+        df = pd.DataFrame(sentiments.neutral_comments)
+        file_name = "reddit_pixel_ecosystem_reviews.csv"
+        df.to_csv(file_name, index=False)
+        df.to_json("reddit_pixel_ecosystem_reviews.json")
+        print(f"\n✅ Data saved successfully to '{file_name}'")
+    else:
+        print("⚠️ No reviews found!")
--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -8,17 +8,17 @@ class SentimentAnalyzer():
         self.negative_sentiments = 0
         self.nuetral_sentiments = 0
         self.negative_comments = []
-        self.nuetral_comments = []
+        self.neutral_comments = []
         self.positive_comments = []
     def assessSentiments(self, reviews:list):
         for review in reviews:
             sentiment = TextBlob(review['self_text']).sentiment
-            if sentiment.subjectivity <=0.5 and sentiment.polarity > 0.25: 
+            if sentiment.subjectivity <=0.5 and sentiment.polarity > 0.05:
                 self.positive_sentiments += 1
                 self.positive_comments.append(f"{review['post_title']}+{review['self_text']}")
-            elif sentiment.subjectivity <= 0.5 and sentiment.polarity < 0.05:
+            elif sentiment.subjectivity <= 0.5 and sentiment.polarity < -0.05:
                 self.negative_sentiments += 1
                 self.negative_comments.append(f"{review['post_title']}+{review['self_text']}")
-            elif sentiment.subjectivity <= 0.5 and (sentiment.polarity >= 0.05 and sentiment.polarity <= 0.25):
+            elif sentiment.subjectivity <= 0.5 and (sentiment.polarity >= -0.05 and sentiment.polarity <= 0.05):
                 self.nuetral_sentiments += 1
-                self.nuetral_comments.append(f"{review['post_title']}+{review['self_text']}")
+                self.neutral_comments.append(f"{review['post_title']}+{review['self_text']}")
